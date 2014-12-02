9fb6485ba59ceb7480b1ac221a2776df9eb8995a
Kiy Hong Tang <kiyhong@gmail.com>
2014-12-02 10:53:37 +0000

    Added first feature test

--- spec/features/search_by_destination_feature_spec.rb
+++ spec/features/search_by_destination_feature_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
-RSpec.feature "search by destination" do
-  scenario do
+
+RSpec.feature "Search" do
+
+  scenario 'Search by destination' do
+    given_there_is_a_destination
+    and_i_am_logged_in_as_a_user
+    when_i_click_on_the_property
+    then_the_property_details_should_appear
+  end
+
+  def given_there_is_a_destination
+  end
+
+  def and_i_am_logged_in_as_a_user
+  end
+
+  def when_i_click_on_the_property
+  end
+
+  def then_the_property_details_should_appear
+    
   end
 end
